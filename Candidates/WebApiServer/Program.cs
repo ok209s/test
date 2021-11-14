33674308064ac7089be71e1d94943a81c9c72a88
ok209s <39025483+ok209s@users.noreply.github.com>
2021-11-14 23:05:44 +0200

    Add files via upload

---
+++ Candidates/WebApiServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://localhost:5000/values");
+                response.EnsureSuccessStatusCode();
+                if(response.IsSuccessStatusCode)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(message);
+                }
+                else
+                {
+                    Console.WriteLine($"response error code : {response.StatusCode}");
+                }
+            }
+        }
+    }
+}
---
+++ Candidates/WebApiServer/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace WebApiServer.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public String Get()
+        {
+
+            var json = new WebClient().DownloadString(@"https://hs-resume-data.herokuapp.com/v3/candidates/all_data_b1f6-acde48001122");
+            StringBuilder sb = new StringBuilder();
+    
+
+            var candidates = JArray.Parse(json);
+            foreach (var candidate in candidates)
+            {
+                sb.Append("Hello " + candidate["contact_info"]["name"]["formatted_name"].ToString());
+                sb.Append(Environment.NewLine);
+                var experiences = candidate["experience"].ToArray();
+
+                foreach (var experience in experiences)
+                {
+                    sb.Append("Worked as: " + experience["title"].ToString() + ", From " + experience["start_date"].ToString() + " To " + experience["end_date"].ToString());
+                    sb.Append(Environment.NewLine);
+                }
+            }
+             
+            return sb.ToString();
+        }
+
+    }
+   
+}
